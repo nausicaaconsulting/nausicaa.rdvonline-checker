03a13dcf66c1dcbddd0937320ca6f2ec8db17e1e
nausicaaconsulting <nausicaaconsulting@gmail.com>
2022-06-13 10:08:16 +0200

    .

--- rdvonline-checker/cli.py
+++ rdvonline-checker/cli.py
@@ -6,6 +6,6 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     postal_code = '33380'
     distance = 50
-    nb_persons = 1
+    nb_persons = 3
 
     rdvonline_web.check_available_date(postal_code, distance, nb_persons)
